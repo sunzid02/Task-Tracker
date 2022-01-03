7bfbe1cbce604b008cbdb45afe7c79c4c7f79c7d
sunzid02 <sunzid02@gmail.com>
2022-01-03 16:36:30 +0100

    delete task

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,8 +8,11 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css']
 })
+
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+
   faTimes = faTimes
 
   constructor() { }
@@ -17,4 +20,7 @@ export class TaskItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task);
+  }
 }
