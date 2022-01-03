c226813d84068419bbb223cf266b20b57ed05be4
sunzid02 <sunzid02@gmail.com>
2022-01-03 15:19:29 +0100

    create task service and subscribe to observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  faCoffee = faCoffee;
 }
